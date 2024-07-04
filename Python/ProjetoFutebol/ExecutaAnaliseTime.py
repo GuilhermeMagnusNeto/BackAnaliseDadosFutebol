d0d3f092485d9c26cf66724aa2758718050ef2ce
Guilherme Magnus Neto <guilherme.magnus.contato@gmail.com>
2024-07-03 22:32:55 -0300

    Update

--- Python/ProjetoFutebol/ExecutaAnaliseTime.py
+++ Python/ProjetoFutebol/ExecutaAnaliseTime.py
@@ -148,7 +148,7 @@ def salvarAnotacao(current_user):
 
 @app.route('/atualizarNota/<int:id_nota>', methods=['PUT'])
 @token_required
-def atualizarNota(id_nota, current_user):
+def atualizarNota(id_nota):
     try:
         # Obtenha os dados enviados pelo cliente
         dados = request.get_json()
